9699afa454505c45f2424a6bef039a168d820359
Ramon Cadenas Urdaneta <cadenasu@shernet.sheridancollege.ca>
2025-07-03 18:54:57 -0400

    Experiment Complete

--- Assets/Scripts/SquareMover.cs
+++ Assets/Scripts/SquareMover.cs
@@ -14,14 +14,33 @@ public class SquareMover : MonoBehaviour
         //Vector3: x(0), y(0), z(0)
         //Type nameOfVariable =
         //Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Vector3 newPosition = transform.position + Vector3.right * 1;
+        //Vector3 newPosition = transform.position + Vector3.right * 1;
         
-        transform.position = newPosition;
+        //transform.position = newPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //speed = 0.01f; - changed to public!
+
+        Vector3 newPosition = transform.position + Vector3.right * speed;
+
+        transform.position = newPosition;
+
+        float xMax = 10f;
+
+        if (transform.position.x > xMax)
+        {
+            speed = -speed;
+        }
+
+
+        float xMin = -10f;
+
+        if (transform.position.x < xMin)
+        {
+            speed = -speed;
+        }
     }
 }
