2239f7d6b98410680beaeaef0421e6fd1a7520f5
Ramon Cadenas Urdaneta <cadenasu@shernet.sheridancollege.ca>
2025-07-03 18:58:09 -0400

    Remembered I should specify what i added for my sake, added the xMaxand xMin floats to set the limits, if statement to get the square moving, and speed float for the bonus challenge of accelerating to a specific speed

--- Assets/Scripts/SquareMover.cs
+++ Assets/Scripts/SquareMover.cs
@@ -6,7 +6,7 @@ public class SquareMover : MonoBehaviour
 {
 
     //Class variables go here:
-    public float speed;
+    public float speed; //bonus challenge!
 
     // Start is called before the first frame update
     void Start()
