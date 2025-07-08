d98da6bc231e7397e27c0ef62039f324b817c092
Ramon Cadenas Urdaneta <cadenasu@shernet.sheridancollege.ca>
2025-07-08 19:04:54 -0400

    Forgot to commit, completed the exercise where the square changes color if it passes the bounds of the screen

--- Assets/Scripts/Chaser.cs
+++ Assets/Scripts/Chaser.cs
@@ -6,6 +6,8 @@ public class Chaser : MonoBehaviour
 {
     public Vector3 newPosition;
     public Camera gameCamera;
+    public SpriteRenderer chaserRenderer;
+
     public float speed;
     Vector3 lastClickedPosition = Vector3.zero;
 
@@ -32,7 +34,7 @@ public class Chaser : MonoBehaviour
         Vector3 mousePositionInWorldSpace = gameCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePositionInWorldSpace.z = 0f;
 
-        if (Input.GetMouseButtonDown(0)) //doublechecked syntax at https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Input.GetMouseButtonDown.html
+        if (Input.GetMouseButtonDown(0)) 
         {
             lastClickedPosition = mousePositionInWorldSpace;
 
@@ -42,7 +44,48 @@ public class Chaser : MonoBehaviour
         Vector3 target = lastClickedPosition;
         Vector3 directionToMove = target - start;
 
-        transform.position = transform.position + directionToMove * speed;
+        //transform.position = transform.position + directionToMove * speed;
 
+        Vector3 chaserPositionInScreenSpace = gameCamera.WorldToScreenPoint(transform.position);
+
+        //Debug.Log("ChaserPosition in ScreenSpace = " + chaserPositionInScreenSpace.ToString());
+
+        float xMin = 0f;
+        float yMin = 0f;
+        float xMax = Screen.width;
+        float yMax = Screen.height;
+
+        //if chaser's x value is less than xMin
+        //if chaser's x value is more than xMax
+        //if chaser's y value is less than yMin
+        //if chaser's y value is more than yMax
+        //THEN:
+        //change the color of the sprite to be red
+
+        //bool xMaxExceeded = if the chaser position goes beyond the right of the screen
+        //bool xMinExceeded = if the chaser position goes beyond the left of the screen
+
+        bool xMaxExceeded = chaserPositionInScreenSpace.x >= xMax;
+        bool xMinExceeded = chaserPositionInScreenSpace.x <= xMin;
+        bool yMaxExceeded = chaserPositionInScreenSpace.y >= yMax;
+        bool yMinExceeded = chaserPositionInScreenSpace.y <= yMin;
+
+        Debug.Log("xMaxExceeded = " + xMaxExceeded.ToString());
+        Debug.Log("xMinExceeded = " + xMinExceeded.ToString());
+        Debug.Log("yMaxExceeded = " + yMaxExceeded.ToString());
+        Debug.Log("yMinExceeded = " + yMinExceeded.ToString());
+
+        Debug.Log("x " + chaserPositionInScreenSpace.x.ToString());
+        Debug.Log("y " + chaserPositionInScreenSpace.y.ToString());
+
+        if (xMaxExceeded || xMinExceeded || yMaxExceeded || yMinExceeded)
+        {
+            //Change the color of the sprite
+            chaserRenderer.color = Color.red;
+        }
+        else
+        {
+            chaserRenderer.color = Color.white;
+        }
     }
 }
