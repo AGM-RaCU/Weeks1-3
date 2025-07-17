f20d176dafbb8a461a7cdda35b9492d24701a9c1
Ramon Cadenas Urdaneta <cadenasu@shernet.sheridancollege.ca>
2025-07-17 18:50:36 -0400

    Fixed up class 4 scripts, did first class 5 exercise with knight and fireball and such

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,33 +4,49 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-
     public GameObject cannonballPrefab;
-    public float cannonballSpeed;
+    public float cannonballMoveDuration;
     public Color cannonballColour;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cannonball test = gameObject.GetComponent<Cannonball>();
+        Debug.Log(test);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         bool leftClick = Input.GetMouseButtonDown(0);
 
-        if(leftClick)
+        if (leftClick)
         {
             GameObject spawnedCannonball = Instantiate(cannonballPrefab, transform.position, Quaternion.identity);
             Debug.Log(spawnedCannonball.name);
             SpriteRenderer cannonballRenderer = spawnedCannonball.GetComponent<SpriteRenderer>();
-            cannonballRenderer.color = cannonballColour;
+
+            //If we have successfully found a renderer, then we can do stuff with it
+            if (cannonballRenderer != null)
+            {
+                cannonballRenderer.color = cannonballColour;
+            }
+            else
+            {
+                //Otherwise don't do stuff with the renderer or else ERRORS!!
+            }
 
             //set the speed? what has the speed?
             Cannonball cannonballScript = spawnedCannonball.GetComponent<Cannonball>();
-            cannonballScript.cannonballMoveDuration = cannonballSpeed;
+            if (cannonballScript == null)
+            {
+                Debug.Log("Cannonball script could not be found.");
+            }
+            else
+            {
+                cannonballScript.moveDuration = cannonballMoveDuration;
+            }
+
         }
     }
 }
