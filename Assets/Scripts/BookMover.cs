2b8cad053bce7a4139e941db7c278fc735cabae1
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-08-02 01:42:30 -0400

    i am losing my mind it literally just works now, i've tried so much random shit just for it to decide to work using exactly the same code i originally had i hate this so much

--- Assets/Scripts/BookMover.cs
+++ Assets/Scripts/BookMover.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.Events;
 
 public class BookMover : MonoBehaviour
 {
 
-    bool MachineOn = false; //Bool binary to change state between moving or not
+    public bool MachineOn = false; //Bool binary to change state between moving or not
+    //public UnityEvent buttonPressed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //buttonOn();
     }
 
     // Update is called once per frame
@@ -37,7 +39,13 @@ public class BookMover : MonoBehaviour
 
     public void buttonOn() //button press turns bool true to begin functionality
     {
-         MachineOn = true; //"the machine turns on"; belt pulls book
-       
+        MachineOn = true; //"the machine turns on"; belt pulls book
+        //Debug.Log("button works");
+        //buttonPressed.Invoke();
+        //Debug.Log("event called");
+        //Vector3 bookPosition = transform.position + Vector3.right * 0.01f; //vector3 defining how the book will move to the right
+        //transform.position = bookPosition;
+        //Debug.Log("literallywhat");
+
     }
 }
